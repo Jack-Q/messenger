5a11009d7b3cee9b43420f251272b6861a479faf
Jack Q <QiaoBo@outlook.com>
2017-04-18 22:14:40 +0800

    add title bar ui

--- app/src/main/java/cn/jackq/messager/MainActivity.java
+++ app/src/main/java/cn/jackq/messager/MainActivity.java
@@ -3,13 +3,16 @@ package cn.jackq.messager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private ViewAnimator mViewAnimator;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mViewAnimator.setDisplayedChild(0);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mViewAnimator.setDisplayedChild(1);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mViewAnimator.setDisplayedChild(2);
                     return true;
             }
             return false;
@@ -37,7 +40,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_dashboard_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        mViewAnimator = (ViewAnimator) findViewById(R.id.view_animator);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
