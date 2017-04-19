7064e441d996b7c773d06c42c3ce5349a4b2d551
Jack Q <QiaoBo@outlook.com>
2017-04-19 15:35:17 +0800

    update reference

--- README.md
+++ README.md
@@ -27,3 +27,9 @@ Client 1 -TCP-\     /-TCP- Client 2
   * Use some form of hole punching using a public server as the go between
   * Use another peer that does have ports open as a proxy for the data (a Supernode).
   * Use a server I host as a proxy to forward the data.
+
+## Async Operation
+http://www.deadcoderising.com/java8-writing-asynchronous-code-with-completablefuture/
+
+## UI Binding 
+http://jakewharton.github.io/butterknife/
--- android/app/src/main/java/cn/jackq/messager/ui/MainActivityHomePage.java
+++ android/app/src/main/java/cn/jackq/messager/ui/MainActivityHomePage.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,15 +16,31 @@ public class MainActivityHomePage {
     private View mRootView;
     private Context mContext;
 
-    @BindView(R.id.status_text) TextView mStatusText;
+    @BindView(R.id.status_text)
+    TextView mStatusText;
 
-    @OnClick(R.id.main_button_call) void buttonClickHandler() {
-        ServerConnection.get().testServer().thenAccept(status -> mStatusText.append(status.getMessage()));
+    @OnClick(R.id.main_message_button)
+    void messageButtonClickHandler(ToggleButton toggleButton) {
+        if (toggleButton.isChecked()) {
+            writeLog("enable audio recording");
+        } else {
+            writeLog("disable audio recording");
+        }
+    }
+
+    @OnClick(R.id.main_button_call)
+    void buttonClickHandler() {
+        ServerConnection.get().testServer().thenAccept(status -> writeLog(status.getMessage()));
     }
 
     public MainActivityHomePage(@NonNull Context context, @NonNull View rootView) {
         mContext = context;
         mRootView = rootView;
         ButterKnife.bind(this, rootView);
+    }
+
+    private void writeLog(String message) {
+        if (!message.endsWith("\n")) message += '\n';
+        mStatusText.append(message);
     }
 }
