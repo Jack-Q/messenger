60735e5b13d66400af57a470211e5c8693c1b3b3
Jack Q <QiaoBo@outlook.com>
2017-04-24 14:57:59 +0800

    add feedback from server

--- server/src/test-peer-client.js
+++ server/src/test-peer-client.js
@@ -3,12 +3,25 @@ import dgram from 'dgram';
 const PORT = 42001;
 
 const sock = dgram.createSocket('udp4');
+
+const remote = {
+  address: undefined,
+  port: undefined
+}
+
 sock.on('message', (msg, rinfo) => {
   console.log(`${rinfo.address}:${rinfo.port}`);
   console.log(`Message: ${msg}`);
+  remote.address = rinfo.address;
+  remote.port = rinfo.port;
 });
 
 sock.on('error', err => {
   console.log(err.message);
 })
+
+setInterval(() => {
+  if (remote.address && remote.port)
+    sock.send('ACK', remote.port, remote.address);
+}, 1000 / 20);
 sock.bind(PORT);
