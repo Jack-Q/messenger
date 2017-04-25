70155fc65da4485dc1f15b377026a1e12d655b0b
Jack Q <QiaoBo@outlook.com>
2017-04-25 13:30:04 +0800

    prep msg type

--- server/src/lib/protocol.js
+++ server/src/lib/protocol.js
@@ -2,7 +2,26 @@ export const PROTOCOL_VERSION = 0x81;
 export const MAXIMUM_PACKET_SIZE = 2048;
   
 export const packetType = {
-  SERVER_CHECK: {type:'SERVER_CHECK', value: 0x01},
+  // Server check
+  SERVER_CHECK: { type: 'SERVER_CHECK', value: 0x01 },
+  SERVER_STATUS: { type: 'SERVER_STATUS', value: 0x02 },
+  // User Create
+  USER_ADD_REQ: { type: 'USER_ADD_REQ', value: 0x03 },
+  USER_ADD_RESP: { type: 'USER_ADD_RESP', value: 0x04 },
+  // User Login
+  USER_LOGIN_REQ: { type: 'USER_LOGIN_REQ', value: 0x05 },
+  USER_LOGIN_RESP: { type: 'USER_LOGIN_RESP', value: 0x06 },
+  // Information Query: query online users, current IPs, etc
+  INFO_QUERY: { type: 'INFO_QUERY', value: 0x07 },
+  INFO_RESP: { type: 'INFO_RESP', value: 0x08 },
+  // Plain Text Conversation
+  MSG_SEND: { type: 'MSG_SEND', value: 0x09 },
+  MSG_RECV: { type: 'MSG_RECV', value: 0x0a },
+  // Call Management
+  CALL_INIT: { type: 'CALL_INIT', value: 0x11 },
+  CALL_WAIT: { type: 'CALL_WAIT', value: 0x12 },
+  CALL_CONN: {type: 'CALL_CONN', valid: 0x13},
+  CALL_END: {type: 'CALL_END', value: 0x14},
 }
 
 export const checkPacket = (buffer, low, high) => {
