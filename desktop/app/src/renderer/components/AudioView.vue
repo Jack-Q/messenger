8c64117f086db462d2441660d62d82de9b18e30e
Jack Q <QiaoBo@outlook.com>
2017-04-29 23:18:18 +0800

    update audio chat ui

--- desktop/app/src/renderer/components/AudioView.vue
+++ desktop/app/src/renderer/components/AudioView.vue
@@ -1,13 +1,34 @@
 <template>
-<div class="page">
-  <div class="title">Audio View</div>
-  <div>click user label on left panel to begin conversation</div>
-</div>
+  <div class="page">
+    <div class="title">Audio View</div>
+    <div class="sub-title">
+      chat with {{peername}}
+    </div>
+    <div class="status">
+      <div class="icon"><ui-icon>call</ui-icon></div>
+      <div>{{status}}</div>
+    </div>
+    <div class="actions">
+      <ui-button type="secondary">
+        <ui-icon>call_end</ui-icon>
+      </ui-button>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      peername: 'Jack Q',
+      status: 'connecting...',
+    };
+  },
+};
+</script>
 <style>
-.page{
+.page {
   display: flex;
-  flex-direction: column;  
+  flex-direction: column;
   font-size: 1.5em;
   text-align: center;
   color: #aaa;
@@ -15,8 +36,22 @@
   height: 100%;
   justify-content: center;
 }
-.title{
+
+.title {
   font-size: 2em;
   margin: 30px;
 }
+
+.status {
+  width: 100%;
+  max-width: 450px;
+  border-top: dashed 1px #aaa;
+  border-bottom: dashed 1px #aaa;
+  margin: 10px auto;
+}
+.status .ui-icon{
+  font-size: 128px;
+  margin: 30px;
+}
+
 </style>
