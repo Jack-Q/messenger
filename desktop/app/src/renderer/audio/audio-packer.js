53507bb20c7c96ed665dabc006f2c4d03c70d059
Jack Q <QiaoBo@outlook.com>
2017-05-02 18:21:08 +0800

    implement audio packer

--- desktop/app/src/renderer/audio/audio-packer.js
+++ desktop/app/src/renderer/audio/audio-packer.js
@@ -1,8 +1,16 @@
+/**
+ * packet format: [index][content]
+ *                ^ 2 byte
+ */
 export default class AudioPacker {
   static pack(data) {
-    return Buffer.from(data);
+    const length = data.frame.length + 2;
+    const buffer = Buffer.allocUnsafe(length);
+    buffer.writeUInt16LE(data.index, 0);
+    data.frame.copy(buffer, 2);
+    return buffer;
   }
   static unpack(buffer) {
-    return { data: buffer };
+    return { index: buffer.readInt16LE(0), data: buffer.slice(2) };
   }
 }
