1a0f2da7e712bb59a7cf368c7c37ee450c373a59
Jack Q <QiaoBo@outlook.com>
2017-06-30 03:22:55 +0800

    use pack for travis

--- desktop/webpack.renderer.config.js
+++ desktop/webpack.renderer.config.js
@@ -129,19 +129,17 @@ if (process.env.NODE_ENV !== 'production') {
    * Apply ESLint
    */
   if (settings.eslint) {
-    rendererConfig.module.rules.push(
-      {
-        test: /\.(js|vue)$/,
-        enforce: 'pre',
-        exclude: /node_modules/,
-        use: {
-          loader: 'eslint-loader',
-          options: {
-            formatter: require('eslint-friendly-formatter'),
-          },
+    rendererConfig.module.rules.push({
+      test: /\.(js|vue)$/,
+      enforce: 'pre',
+      exclude: /node_modules/,
+      use: {
+        loader: 'eslint-loader',
+        options: {
+          formatter: require('eslint-friendly-formatter'),
         },
       },
-    );
+    });
   }
 }
 
