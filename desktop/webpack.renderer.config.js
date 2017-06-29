974276a0a29f3a3fdacbee30e0b245c787f39b4a
Jack Q <QiaoBo@outlook.com>
2017-06-30 03:39:17 +0800

    more compatibility to older syntax

--- desktop/webpack.renderer.config.js
+++ desktop/webpack.renderer.config.js
@@ -149,19 +149,17 @@ if (process.env.NODE_ENV !== 'production') {
 if (process.env.NODE_ENV === 'production') {
   rendererConfig.devtool = '';
 
-  rendererConfig.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }), // ,
+  rendererConfig.plugins.push(new webpack.DefinePlugin({
+    'process.env.NODE_ENV': '"production"',
+  }), new webpack.LoaderOptionsPlugin({
+    minimize: true,
+  }));
+    // uglify js plugin
     // new webpack.optimize.UglifyJsPlugin({
     //   compress: {
     //     warnings: false,
     //   },
     // })
-  );
 }
 
 module.exports = rendererConfig;
